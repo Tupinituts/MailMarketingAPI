88f9100d3fae50fb6e9837af2a0c1ac90db351a9
fecamargo <felipe.camargo@mercadolivre.com>
2021-09-17 21:45:45 -0300

    first commit

---
+++ src/main/java/br/com/tupinituts/MailMarketing/MailMarketingApplication.java
@@ -0,0 +1,13 @@
+package br.com.tupinituts.MailMarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailMarketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tupinituts/MailMarketing/MailMarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tupinituts.MailMarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailMarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
